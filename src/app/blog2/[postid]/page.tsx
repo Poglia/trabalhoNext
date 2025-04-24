7436ceee830b6680946a53e1bb607252647222f2
Poglia <pedro.poglia@hotmail.com>
2025-04-24 14:16:06 -0300

    consultando dados de api externa

---
+++ src/app/blog2/[postid]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Post } from "@/types/Post";
+
+type Props = {
+    params: {
+        postid: string;
+    };  
+}
+const Page = async({ params }: Props) => {
+
+    const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postid}`);
+    const post: Post = await postResponse.json();
+
+    if (!post.id) {
+        return <div>Post não Encontrado</div>;
+    }
+
+    return (
+        <div className="text-center">
+            <div>ID do post: {params.postid}</div>
+            <h1 className="text-3xl my-3 uppercase">{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    );
+}   
+
+export default Page;
