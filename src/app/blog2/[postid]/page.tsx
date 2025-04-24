902e54023a598c3ff4bcf41f34666528ffd767ce
Poglia <pedro.poglia@hotmail.com>
2025-04-24 16:27:50 -0300

    alterando metadata dinamicamente

--- src/app/blog2/[postid]/page.tsx
+++ src/app/blog2/[postid]/page.tsx
@@ -1,31 +1,39 @@
 import { Post } from "@/types/Post";
 import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "Blog",
-  };
-
 type Props = {
-    params: {
-        postid: string;
-    };  
-}
-const Page = async({ params }: Props) => {
+  params: {
+    postid: string;
+  };
+};
 
-    const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postid}`);
-    const post: Post = await postResponse.json();
+export const  generateMetadata = async({ params }: Props):Promise<Metadata> => {
+  const postResponse = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postid}`
+  );
+  const post: Post = await postResponse.json();
 
-    if (!post.id) {
-        return <div>Post não Encontrado</div>;
-    }
+  return {
+    title: post.title,
+  };
+};
 
-    return (
-        <div className="text-center">
-            <div>ID do post: {params.postid}</div>
-            <h1 className="text-3xl my-3 uppercase">{post.title}</h1>
-            <p>{post.body}</p>
-        </div>
-    );
-}   
+const Page = async ({ params }: Props) => {
+  const postResponse = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postid}`
+  );
+  const post: Post = await postResponse.json();
+
+  if (!post.id) {
+    return <div>Post não Encontrado</div>;
+  }
+
+  return (
+    <div className="text-center">
+      <div>ID do post: {params.postid}</div>
+      <h1 className="text-3xl my-3 uppercase">{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
 export default Page;
