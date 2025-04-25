b29a6da54019d4eb25cf86eb61985f1a41d0c9c2
Poglia <pedro.poglia@hotmail.com>
2025-04-24 21:55:49 -0300

    req server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 
+import { Posts } from "@/components/post";
 import { Metadata } from "next";
 
 const Page = () => {
   return (
     <div>
       <h1>Pagina Inicial</h1>
+
+      <Posts />
     </div>
   );
 }
---
+++ src/components/post.tsx
@@ -0,0 +1,14 @@
+import { Post } from "@/types/Post";
+
+export const Posts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts: Post[] = await res.json();
+    return (
+      <ul>
+        {posts.map(item => (
+          <li key={item.id}>{item.title}</li>)
+        )}
+        
+      </ul>
+    );
+}
